84d4c8697bad83cf8946daaf5d81110702c0e271
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-04-30 14:19:34 +0400

    feat(TableHeader.js): remove comment

--- src/components/TableHeader/TableHeader.js
+++ src/components/TableHeader/TableHeader.js
@@ -2,22 +2,6 @@ import { createElement } from "../../helpers/createElement.js";
 import TableHeaderInfo from "./TableHeaderInfo.js";
 
 export default function TableHeader() {
-  /*
-  
-  <header>
-    <img
-      src="./src/assets/images/leagues/premier-league.png"
-      alt=""
-      class="header__logo"
-    />
-    <div class="header__info">
-      <h2 class="title">Premier League</h2>
-      <p>Season 2022 - 2023</p>
-    </div>
-  </header>
-  
-  */
-
   const TableHeaderImage = createElement("img", {
     src: "./src/assets/images/leagues/premier-league.png",
     alt: "Premier League",
