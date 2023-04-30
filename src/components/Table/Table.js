38e611bced25258e1c6443624ca695899ddfe0e3
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-04-30 13:27:47 +0400

    feat(TableLiner): add component

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,6 +1,12 @@
 import { createElement } from "../../helpers/createElement.js";
 import TableHeader from "../TableHeader/TableHeader.js";
+import TableLiner from "../TableLiner/TableLiner.js";
 
 export default function Table() {
-  return createElement("section", { class: "table" }, TableHeader());
+  return createElement(
+    "section",
+    { class: "table" },
+    TableHeader(),
+    TableLiner()
+  );
 }
