f305357550ea459513becfc6fe318323a57725af
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-04-30 12:18:35 +0400

    fix(index.js): remove comments

--- index.js
+++ index.js
@@ -3,24 +3,4 @@ import aplMatches from "./src/assets/data/matches/apl.json" assert { type: "json
 import { matchesResultOption } from "./src/constants/matches.constants.js";
 import getClubsStat from "./src/services/matches.services.js";
 
-// console.log("aplClubs:::", aplClubs);
-// console.log("aplMatches:::", aplMatches);
-
-/*
-
-{
-  'Team Name': {
-    mp: 0,
-    w: 0,
-    d: 0,
-    l: 0
-    gf: 0,
-    ga: 0
-    gd: 0,
-    pts: 0
-  }
-}
-
-*/
-
 console.log(getClubsStat(aplMatches.matches, matchesResultOption));
