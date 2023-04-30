38d71b1906748c6a8a9b2226f4685e4b6070fa8b
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-04-30 14:06:08 +0400

    feat(getDatas): add getTable service

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,12 @@
 import { createElement } from "../../helpers/createElement.js";
+import { getTable } from "../../services/getDatas.services.js";
 import Club from "../Club/Club.js";
 import TableHeader from "../TableHeader/TableHeader.js";
 import TableLiner from "../TableLiner/TableLiner.js";
 
 export default function Table() {
+  console.log(getTable());
+
   return createElement(
     "section",
     { class: "table" },
