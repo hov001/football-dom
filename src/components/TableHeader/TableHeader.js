912b0b685c6d742792018bc50601c96eca384352
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-04-30 12:59:23 +0400

    feat(TableHeader): add image

--- src/components/TableHeader/TableHeader.js
+++ src/components/TableHeader/TableHeader.js
@@ -18,8 +18,19 @@ export default function TableHeader() {
   
   */
 
-  return TableHeaderInfo({
-    title: "Premier league",
-    description: "Season 2021",
+  const TableHeaderImage = createElement("img", {
+    src: "./src/assets/images/leagues/premier-league.png",
+    alt: "Premier League",
+    class: "header__logo",
   });
+
+  return createElement(
+    "header",
+    {},
+    TableHeaderImage,
+    TableHeaderInfo({
+      title: "Premier league",
+      description: "Season 2021",
+    })
+  );
 }
